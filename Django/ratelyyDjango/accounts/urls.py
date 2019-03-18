7996c74a2e52c0cb0bc8d85056b43fc2af5475b8
Anthony J. Sherrill Jr <a@sherrill.de>
2019-03-18 11:30:23 +0100

    working currently at the reset password

--- Django/ratelyyDjango/accounts/urls.py
+++ Django/ratelyyDjango/accounts/urls.py
@@ -1,10 +1,22 @@
 from django.conf.urls import url
 from . import views
-from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.views import (
+LoginView,
+LogoutView,
+PasswordResetView,
+PasswordResetDoneView,
+# password_reset_confirm,
+)
 
 urlpatterns = [
     url(r"^$", views.home),
     url(r"^login/$", LoginView.as_view(template_name="accounts/login.html"), name="login"),
     url(r"^logout/$", LogoutView.as_view(template_name="accounts/logout.html"), name="logout"),
     url(r"^register$", views.register, name="register"),
+    url(r"^profile/$", views.view_profile, name="view_profile"),
+    url(r"profile/edit/$", views.edit_profile, name="edit_profile"),
+    url(r"change-password/$", views.change_password, name="change_password"),
+    url(r"^reset-password/$", PasswordResetView.as_view(), name="reset_password"),
+    url(r"^reset-password/done$", PasswordResetDoneView.as_view(), name="password_reset_done"),
+    # url(r"^reset-password/confirm$", password_reset_confirm, name="password_reset_confirm"),
 ]
--- Django/ratelyyDjango/accounts/views.py
+++ Django/ratelyyDjango/accounts/views.py
@@ -2,7 +2,13 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from accounts.forms import (
+    RegistrationFrom,
+    EditProfileForm,
+)
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserChangeForm, PasswordChangeForm
+from django.contrib.auth import update_session_auth_hash
 
 # Create your views here.
 # All logic goes thru the views. If you want to access e.g. data from a database
@@ -19,12 +25,43 @@ def home(request):
 
 def register(request):
     if request.method == "POST":
-        form = UserCreationForm(request.POST)
+        form = RegistrationFrom(request.POST)
         if form.is_valid():
             form.save()
             return redirect("/account")
     else:
-        form = UserCreationForm()
+        form = RegistrationFrom()
 
         args = {"form": form}
         return render(request, "accounts/reg_form.html", args)
+
+def view_profile(request):
+    args = {"user": request.user}
+    return render(request, "accounts/profile.html", args)
+
+def edit_profile(request):
+    if request.method == "POST":
+        form = EditProfileForm(request.POST, instance=request.user)
+
+        if form.is_valid():
+            form.save()
+            return redirect("/account/profile")
+    else:
+        form = EditProfileForm(instance = request.user)
+        args = {"form": form}
+        return render(request, "accounts/edit_profile.html", args)
+
+def change_password(request):
+    if request.method == "POST":
+        form = PasswordChangeForm(data = request.POST, user = request.user)
+
+        if form.is_valid():
+            form.save()
+            update_session_auth_hash(request, form.user)
+            return redirect("/account/profile")
+        else:
+            return redirect("/account/change-password")
+    else:
+        form = PasswordChangeForm(user = request.user)
+        args = {"form": form}
+        return render(request, "accounts/change_password.html", args)
