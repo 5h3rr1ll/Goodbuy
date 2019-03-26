fe935685b4deb9983a44fa7ddf29cb4d069d17bd
Anthony J. Sherrill Jr <anthony.sherrill@code.berlin>
2019-03-27 00:56:10 +0100

    Posts become display at /home and are sorted by time

--- Django/ratelyyDjango/home/forms.py
+++ Django/ratelyyDjango/home/forms.py
@@ -1,4 +1,11 @@
 from django import forms
+from home.models import Post
 
-class HomeForm(forms.Form):
-    post = forms.CharField()
+class HomeForm(forms.ModelForm):
+    # needed to uncommand this line because otherweise the input field appears
+    # twice
+    # Foo = forms.CharField()
+
+    class Meta:
+        model = Post
+        fields = ("post",)
--- Django/ratelyyDjango/home/views.py
+++ Django/ratelyyDjango/home/views.py
@@ -2,12 +2,31 @@
 from __future__ import unicode_literals
 
 from django.views.generic import TemplateView
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from home.forms import HomeForm
+from home.models import Post
 
 class HomeView(TemplateView):
     template_name = "home/home.html"
 
     def get(self, request):
         form = HomeForm()
-        return render(request, self.template_name, {"form":form})
+        posts = Post.objects.all().order_by("-created")
+
+        args = {"form":form, "posts":posts}
+        return render(request, self.template_name, args)
+
+    def post(self, request):
+        form = HomeForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.user = request.user
+            post.save()
+            # is a safty method to make sure that no bad code is in the forms
+            # like a SQL injection
+            text = form.cleaned_data["post"]
+            form = HomeForm()
+            return redirect("home:home")
+
+        args = {"form":form, "text":text}
+        return render(request, self.template_name, args)
