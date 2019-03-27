4a223b3486da57732127593eaac2b0a9967d18bc
Anthony J. Sherrill Jr <anthony.sherrill@code.berlin>
2019-03-27 20:22:12 +0100

    added Friends model to create many to many relationship whithin users

--- Django/ratelyyDjango/home/views.py
+++ Django/ratelyyDjango/home/views.py
@@ -4,7 +4,8 @@ from __future__ import unicode_literals
 from django.views.generic import TemplateView
 from django.shortcuts import render, redirect
 from home.forms import HomeForm
-from home.models import Post
+from home.models import Post, Friend
+from django.contrib.auth.models import User
 
 class HomeView(TemplateView):
     template_name = "home/home.html"
@@ -12,8 +13,9 @@ class HomeView(TemplateView):
     def get(self, request):
         form = HomeForm()
         posts = Post.objects.all().order_by("-created")
+        users = User.objects.exclude(id=request.user.id)
 
-        args = {"form":form, "posts":posts}
+        args = {"form":form, "posts":posts, "users":users}
         return render(request, self.template_name, args)
 
     def post(self, request):
@@ -30,3 +32,13 @@ class HomeView(TemplateView):
 
         args = {"form":form, "text":text}
         return render(request, self.template_name, args)
+
+def change_friends(request, operation, pk):
+    new_friend = user.object.get(pk=pk)
+    if operation == "add":
+        Friend.make_friend(request.user, new_friend)
+    elif operation == "remove":
+        Friend.lose_friend(request.user, new_friend)
+
+
+    return redirect("home:home")
