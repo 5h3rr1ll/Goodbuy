a5f2421f5299874b821dc0f167f5783733b64f9e
Anthony Sherrill <jigga@Merals-MBP-2.fritz.box>
2019-04-21 03:12:00 +0200

    cleaned up code in mvpScanWebApp views.py. Changed gtin of model product

--- Django/ratelyyDjango/mvpScanWebApp/views.py
+++ Django/ratelyyDjango/mvpScanWebApp/views.py
@@ -2,39 +2,28 @@ from django.shortcuts import render, redirect
 from mvpScanWebApp import views
 from ratelyyDatabase.models import Product
 from mvpScanWebApp.forms import AddNewProductForm
+from django.contrib import messages
+
 # Create your views here.
 
 def gtin(request):
-    numbers = [1,2,3,4,5]
-    name = "Anthony Sherrill"
-
-    # myname is the name we will use in the template and the value in the
-    # dict is the variable within this function
-    # args takes all variabels you want to display on the rendered site
-    args = {
-        "myName": name, "numbers": numbers
-    }
-    return render(request, "mvpScanWebApp/gtin.html", args)
+    return render(request, "mvpScanWebApp/gtin.html")
 
 def add(request, code):
-    # form = AddNewProductForm(initial={"gtin":code})
-    # args = {
-    #     "code":code,
-    #     "form":form,
-    # }
-    # return render(request, "mvpScanWebApp/add.html",args)
-
     if request.method == "POST":
         form = AddNewProductForm(request.POST)
         if form.is_valid():
             form.save()
             return redirect("/gtin")
         else:
-            return redirect("/accounts/profile")
+            # TODO: need a better logic for too long codes inserted
+            form = AddNewProductForm(initial={"gtin":code})
+            args = {"form": form}
+            return render(request, "mvpScanWebApp/add.html", args)
     else:
         form = AddNewProductForm(initial={"gtin":code})
         args = {"form": form}
-        return render(request, "accounts/reg_form.html", args)
+        return render(request, "mvpScanWebApp/add.html", args)
 
 def show(request, code):
     product = Product.objects.get(gtin=code)
