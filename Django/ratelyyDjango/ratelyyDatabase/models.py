73e79b2c27cfe44280d9a3a939cc81ac3dec7f46
Anthony J. Sherrill Jr <anthony.sherrill@code.berlin>
2019-04-29 14:47:07 +0200

    Calc the total rating

--- Django/ratelyyDjango/ratelyyDatabase/models.py
+++ Django/ratelyyDjango/ratelyyDatabase/models.py
@@ -45,7 +45,7 @@ class Concern(models.Model):
 
     def __str__(self):
         return self.name
-        
+
 class Rating(models.Model):
     id = models.AutoField(primary_key=True)
     humans = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(10)], null=True)
@@ -53,9 +53,8 @@ class Rating(models.Model):
     animals = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(10)], null=True)
     concern = models.ForeignKey(Concern, models.SET_NULL, null=True, blank=True)
 
-
     class Meta:
-        managed = True  
+        managed = True
         db_table = "ratings"
 
 class Company(models.Model):
