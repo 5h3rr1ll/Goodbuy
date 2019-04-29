40cb3f52917316a74a46a392e1f8154c075e6862
Anthony J. Sherrill Jr <anthony.sherrill@code.berlin>
2019-04-29 21:18:03 +0200

    catch the case a product has no rating at all in show.html

--- Django/ratelyyDjango/mvpScanWebApp/views.py
+++ Django/ratelyyDjango/mvpScanWebApp/views.py
@@ -24,7 +24,10 @@ def add(request, code):
             product = Product.objects.get(code=code)
             concern = Concern.objects.get(name=product.concern)
             rating = Rating.objects.get(concern=concern.id)
-            total_rating = round((rating.animals+rating.humans+rating.environment)/3)*10
+            if rating.animals == None or rating.humans == None  or rating.environment == None:
+                total_rating = 0
+            else:
+                total_rating = round((rating.animals+rating.humans+rating.environment)/3)*10
             args = {
                 "product":product,
                 "rating_result":total_rating,
