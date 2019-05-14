7a29abc882b67980f96a9d798c8d9ba7268a6de3
Anthony J. Sherrill Jr <anthony.sherrill@code.berlin>
2019-05-15 01:46:45 +0200

    added logic to check if user member of ProductGroup based on that set checked to false or true

--- codeScanner/views.py
+++ codeScanner/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, render_to_response
 from ratelyyDatabase.models import Product, Concern, Rating
 from codeScanner.forms import AddNewProductForm
+from django.contrib.auth.models import User, Group
 
 # Create your views here.
 
@@ -11,13 +12,23 @@ def add(request, code):
     if request.method == "POST":
         form = AddNewProductForm(request.POST)
         if form.is_valid():
-            '''commit=False allows you to modify the resulting object before it is
-            actually saved to the database. Source: https://stackoverflow.com/questions/2218930/django-save-user-id-with-model-save?noredirect=1&lq=1'''
+            '''commit=False allows you to modify the resulting object before it
+            is actually saved to the database. Source:
+            https://stackoverflow.com/questions/2218930/django-save-user-id-with-model-save?noredirect=1&lq=1'''
             product = form.save(commit=False)
             product.added_by = request.user
+            if request.user.groups.filter(name="ProductGroup").exists():
+                print("Worked")
+                product.checked = True
+                product.checked_by = request.user
+                product.save()
+                return redirect("/code")
+            else:
+                print("Doh!")
+                product.checked = False
+                product.save()
+                return redirect("/code")
 
-            product.save()
-            return redirect("/code")
         return render_to_response('codeScanner/add.html', {'form': form})
     else:
         try:
