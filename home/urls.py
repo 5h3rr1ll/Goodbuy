7c8bc129a464c7cf6898e128f54fed70b1d32513
Anthony J. Sherrill Jr <anthony.sherrill@code.berlin>
2019-05-15 13:49:05 +0200

    Changed home.urls from uls to path

--- home/urls.py
+++ home/urls.py
@@ -1,11 +1,11 @@
-from django.conf.urls import url
+from django.urls import path
 from home.views import HomeView
 from . import views
 
 app_name = "home"
 
 urlpatterns = [
-    url(r"^$", HomeView.as_view(), name="home"),
-    url(r"^connect/(?P<operation>.*)/(?P<pk>\d+)/$", views.change_friends, name="change_friends"),
-    url(r"^rating/<code>$", views.rating, name="rating"),
+    path("", HomeView.as_view(), name="home"),
+    path("connect/(?P<operation>.*)/(?P<pk>\d+)/", views.change_friends, name="change_friends"),
+    path("rating/<code>", views.rating, name="rating"),
 ]
