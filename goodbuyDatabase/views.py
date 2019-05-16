bb0394c35ed4dd5175bbab3ecf6653479cf08188
Anthony J. Sherrill Jr <anthony.sherrill@code.berlin>
2019-05-16 15:54:28 +0200

    Images in product_list.html are now clickable

--- goodbuyDatabase/views.py
+++ goodbuyDatabase/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render, redirect, render_to_response
+
 from goodbuyDatabase.forms import AddNewProductForm
 from goodbuyDatabase.models import Product
 
 def add(request, code):
     if request.method == "POST":
         form = AddNewProductForm(request.POST, request.FILES)
-        print(request.FILES)
         image = request.FILES["image"]
-        print("Image Infos= Name:",image.name,"size:",image.size,"URL:")
+        print("Image Infos= Name:",image.name,"size:",image.size)
         if form.is_valid():
             '''commit=False allows you to modify the resulting object before it
             is actually saved to the database. Source:
@@ -15,13 +15,11 @@ def add(request, code):
             product = form.save(commit=False)
             product.added_by = request.user
             if request.user.groups.filter(name="ProductGroup").exists():
-                print("Worked")
                 product.checked = True
                 product.checked_by = request.user
                 product.save()
                 return redirect("/code")
             else:
-                print("Doh!")
                 product.checked = False
                 product.save()
                 return redirect("/code")
