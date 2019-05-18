d7520d30e0159581ac644e298c7cd3d55b9ff5f5
Anthony J. Sherrill Jr <anthony.sherrill@code.berlin>
2019-05-18 12:17:15 +0200

    Set Djangos SECRET_KEY to local environment variable

--- goodbuy/settings.py
+++ goodbuy/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'f81ef2_sh7+)vg+&#nrxjo%@xa1mbep#mofsnvcob&9$uu9jb4'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
