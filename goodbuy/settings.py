d47fc705cdec239547a91c27107fe9f9fb0b729b
Anthony J. Sherrill Jr <anthony.sherrill@code.berlin>
2019-05-19 20:02:54 +0200

    Setup email server with local env security creditentials,set new urls

--- goodbuy/settings.py
+++ goodbuy/settings.py
@@ -180,9 +180,12 @@ LOGIN_EXEMPT_URLS = (
     r"^accounts/reset-password/complete/$",
 )
 
-# TODO: Need to set up a proper email server here
-EMAIL_HOST = 'localhost'
-EMAIL_PORT = 1025
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
 
 # Activate Django-Heroku.
 django_heroku.settings(locals())
--- goodbuy/urls.py
+++ goodbuy/urls.py
@@ -12,15 +12,41 @@ from home import views as home_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", home_views.start_screen, name="home"),
-    path("login/", auth_views.LoginView.as_view(template_name="accounts/login.html"), name="login"),
-    path("logout/", auth_views.LogoutView.as_view(template_name="accounts/logout.html"), name="logout"),
+    path("login/", auth_views.LoginView.as_view(
+        template_name="accounts/login.html"),
+        name="login"),
+    path("logout/", auth_views.LogoutView.as_view(
+        template_name="accounts/logout.html"),
+        name="logout"),
     path("register/", acccount_views.register, name="register"),
-    path('profile/', acccount_views.view_profile, name="profile"),
-    path("accounts/", include(("accounts.urls","accounts"),namespace="accounts")),
-    path('code/', include(('codeScanner.urls',"codeScanner"),namespace="codeScanner")),
-    path('mvpLogoGrab/', include(('mvpLogoGrab.urls',"logograb"),namespace="logograb")),
-    path('goodbuyDatabase/', include(('goodbuyDatabase.urls',"goodbuyDatabase"),namespace="goodbuyDatabase")),
+    path('profile/', acccount_views.user_profile, name="user_profile"),
+    path("accounts/", include(
+        ("accounts.urls","accounts"),
+        namespace="accounts")),
+    path("password-reset/", auth_views.PasswordResetView.as_view(
+        template_name="accounts/password_reset.html"),
+        name="password_reset"),
+    path("password-reset/done/", auth_views.PasswordResetDoneView.as_view(
+        template_name="accounts/password_reset_done.html"),
+        name="password_reset_done"),
+    path("password-reset-confirm/<uidb64>/<token>/", auth_views.PasswordResetConfirmView.as_view(
+        template_name="accounts/password_reset_confirm.html"),
+        name="password_reset_confirm"),
+    path("password-reset-complete/", auth_views.PasswordResetCompleteView.as_view(
+        template_name="accounts/password_reset_complete.html"),
+        name="password_reset_complete"),
+    path("home/", include(("home.urls","home"),namespace="home")),
+    path('code/', include(
+        ('codeScanner.urls',"codeScanner"),
+        namespace="codeScanner")),
+    path('mvpLogoGrab/', include(
+        ('mvpLogoGrab.urls',"logograb"),
+        namespace="logograb")),
+    path('goodbuyDatabase/', include(
+        ('goodbuyDatabase.urls',"goodbuyDatabase"),
+        namespace="goodbuyDatabase")),
 ]
 
 if settings.DEBUG == True:
-    urlpatterns +=  static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns +=  static(settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT)
