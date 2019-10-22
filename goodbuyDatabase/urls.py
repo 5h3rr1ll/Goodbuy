fbad8c49c627f165e328b09c4aabacf7ddd6c8ed
Darjusch <Darjusch.schrand@code.berlin>
2019-10-22 15:51:36 +0200

    api

--- goodbuyDatabase/urls.py
+++ goodbuyDatabase/urls.py
@@ -12,4 +12,5 @@ urlpatterns = [
     path("list_all/", goodbuyDatabase_views.product_list, name="product_list"),
     path("list_codes/<list>/", goodbuyDatabase_views.show_list_of_codes, name="show_list_of_codes"),
     path("product/<int:pk>/details", goodbuyDatabase_views.ProductDetailView.as_view(), name="product_detail"),
+    path("api/<str:code>", goodbuyDatabase_views.receive_code, name="receive_code"),
 ]
