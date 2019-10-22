6db3c74ded2143a11396fe0ec627af68af4a2587
Anthony <anthony.sherrill@code.berlin>
2019-10-22 15:47:04 +0200

    unneeded files

--- collectDataApp/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from collectDataApp import views
-
-app_name = "collectDataApp"
-
-urlpatterns = [
-    path('', views.addProductView, name='addProductView'),
-]
--- collectDataApp/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.contrib.auth.decorators import login_required
-
-# Create your views here.
-
-@login_required
-def addProductView(request):
-    return render(request, "collectDataApp/addProductView.html")
