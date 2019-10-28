566c4f23d82071070e6b66ff91cbb642152e125e
Anthony <anthony.sherrill@code.berlin>
2019-10-28 13:35:14 +0100

    changed local db name

--- settings/local.py
+++ settings/local.py
@@ -5,7 +5,7 @@ DEBUG = True
 DATABASES = {
     "default" : {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": "postgres",
+        "NAME": "local_goodbuy_db",
         "HOST": "127.0.0.1",
         "Port": "5432",
     }
