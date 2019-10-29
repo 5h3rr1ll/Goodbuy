cb26567f3183caf93b877ebb7f2c3c57b6dae0e1
Anthony <anthony.sherrill@code.berlin>
2019-10-29 19:02:03 +0100

    arranged the settings so that you always proactive need to choose a DB

--- settings/base.py
+++ settings/base.py
@@ -86,24 +86,7 @@ WSGI_APPLICATION = 'goodbuy.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'USER': os.environ.get("DATABASEUSER"),
-        'NAME': os.environ.get("DATABASENAME"),
-        'PASSWORD': os.environ.get("DATABASEPW"),
-        'HOST': os.environ.get("DATABASEHOST"),
-        'PORT': '5432',
-    },
-    'test_database': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'USER': "bgwvoxvzwbfeog",
-        'NAME': "d5u4p788nmhdm",
-        'PASSWORD': "8ec85dfdb4c4116fc4bdf80e613bbba3042970f7b6b7ade6fdce511d9b5481cf",
-        'HOST': "ec2-54-246-100-246.eu-west-1.compute.amazonaws.com",
-        'PORT': '5432',
-    }
-}
+DATABASES = {}
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
