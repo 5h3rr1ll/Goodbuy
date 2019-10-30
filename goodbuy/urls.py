c2fcc797895002ad0349d01306fe4105d1428278
Darjusch <Darjusch.schrand@code.berlin>
2019-10-30 15:23:43 +0100

    working brandScraper for one brand

--- goodbuy/urls.py
+++ goodbuy/urls.py
@@ -8,6 +8,7 @@ from accounts import views as acccount_views
 from home import views as home_views
 from goodbuyDatabase import views as gd_views
 from scraper import views as scraper_views
+from brandScraper import views as brand_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
