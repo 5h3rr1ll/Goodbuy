37f5cdce53792d53fd8ce035b191ef26b5580870
Anthony <anthony.sherrill@code.berlin>
2019-11-06 14:09:57 +0100

    starts local posgreSQL DB automatically upfront

--- settings/local.py
+++ settings/local.py
@@ -1,9 +1,16 @@
+import os
+
 from .base import *
 
-ALLOWED_HOSTS = ["127.0.0.1","localhost",]
+os.system("pg_ctl -D /usr/local/var/postgres start")
+
+ALLOWED_HOSTS = [
+    "127.0.0.1",
+    "localhost",
+]
 DEBUG = True
 DATABASES = {
-    "default" : {
+    "default": {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": "local_goodbuy_db",
         "HOST": "127.0.0.1",
