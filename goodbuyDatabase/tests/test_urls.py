3074325457e40eb652b8a2d6aa63841ea638c53b
Darjusch <Darjusch.schrand@code.berlin>
2019-11-11 13:01:24 +0100

    urls tests

---
+++ goodbuyDatabase/tests/test_urls.py
@@ -0,0 +1,26 @@
+from django.test import TestCase
+from django.urls import reverse, resolve
+from goodbuyDatabase.views import is_in_own_database, add_product, product_list, show_list_of_codes
+from django.contrib.auth.models import User
+from django.test import Client
+
+class TestUrls(TestCase):
+   
+
+   # Resolves checks if the url is called, the assoziated function is found
+
+    def test_is_in_own_database(self):
+        url = reverse('goodbuyDatabase:is_in_own_database', args=['4000582185399'])
+        self.assertEqual(resolve(url).func, is_in_own_database)
+
+    def test_add_product(self):
+        url = reverse('goodbuyDatabase:add_product', args=['4000582185399'])
+        self.assertEqual(resolve(url).func, add_product)
+    
+    def test_product_list(self):
+        url = reverse('goodbuyDatabase:product_list')
+        self.assertEqual(resolve(url).func, product_list)
+
+    def test_show_list_of_codes(self):
+        url = reverse('goodbuyDatabase:show_list_of_codes', args=['4000582185399'])
+        self.assertEqual(resolve(url).func, show_list_of_codes)
