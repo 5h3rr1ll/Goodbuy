1669387387f83ebf8a40e6e84fadfeb710980537
Anthony <anthony.sherrill@code.berlin>
2019-11-11 22:05:12 +0100

    added new scraper kellogs,mars,mondelez and pepsico

---
+++ scraper/mondelez(kraft)_brands.py
@@ -0,0 +1,43 @@
+import requests
+from bs4 import BeautifulSoup
+
+import re
+
+class MondelezWikiScraper:
+    def __init__(self):
+        self.url = "https://en.wikipedia.org/wiki/List_of_Kraft_brands"
+        self.request = requests.get(self.url)
+        self.soup = BeautifulSoup(self.request.content, "html.parser")
+
+    def save_brand(self, brand):
+        data = {
+            "name": brand,
+            "corporation": "Mondelez",
+        }
+        requests.post(
+            "http://localhost:8000/goodbuyDatabase/save_brand/", json=data,
+        )
+
+
+    def get_rid_of(self, bs_object):
+        for list_element in bs_object.findAll("li"):
+            link_text = list_element.get_text()
+            special_char = re.findall("[\][–)(,}:]|[0-9]{4}", link_text)
+            try:
+                print(link_text.split(special_char[0])[0])
+                self.save_brand(link_text.split(special_char[0])[0].strip())
+            except:
+                print(link_text)
+                self.save_brand(link_text.strip())
+
+    def get_all_categories(self):
+        list_of_div_locations = {
+            "Current brands" : "#mw-content-text > div > div.div-col.columns.column-width",
+        }
+        for category, div_location in list_of_div_locations.items():
+            print(f"\nCategory: {category}")
+            self.get_rid_of(self.soup.select_one(div_location))
+
+
+mondelez_wiki = MondelezWikiScraper()
+mondelez_wiki.get_all_categories()
