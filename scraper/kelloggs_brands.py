86eca090d405c58cf142dbeee7480f3065cd6625
Anthony <anthony.sherrill@code.berlin>
2019-11-12 21:13:08 +0100

    refactored kellogs lookup

--- scraper/kelloggs_brands.py
+++ scraper/kelloggs_brands.py
@@ -1,10 +1,12 @@
+import re
+
 import requests
 from bs4 import BeautifulSoup
 
-import re
-
 
 class KelloggsWikiScraper:
+    """Returns brands of Kellog's"""
+
     def __init__(self):
         self.url = "https://en.wikipedia.org/wiki/Kellogg%27s"
         self.request = requests.get(self.url)
@@ -22,7 +24,7 @@ class KelloggsWikiScraper:
     def clean_up_brand_name(self, bs_object):
         try:
             if bs_object.findAll("li") == []:
-                print('empty Error')
+                print("empty Error")
             else:
                 for list_element in bs_object.findAll("li"):
                     link_text = list_element.get_text()
@@ -34,22 +36,20 @@ class KelloggsWikiScraper:
                         print(link_text)
                         self.save_brand(link_text.strip())
         except AttributeError as e:
-            print(str(e), ' div changed position ')
-
+            print(str(e), " div changed position ")
 
     def get_all_div_location(self):
         div_locations_list = {
-            "Products" : "#mw-content-text > div > div:nth-child(26)",
-            "Cereal" : "#mw-content-text > div > div:nth-child(30)",
+            "Products": "#mw-content-text > div > div:nth-child(26)",
+            "Cereal": "#mw-content-text > div > div:nth-child(30)",
         }
         return div_locations_list
 
     def iterate_over_list(self, lst):
-        for Category, div_location in lst.items():
-            print(f"\nBrands starting with: {Category}")
+        for category, div_location in lst.items():
+            print(f"\n{category}:")
             div_location = self.soup.select_one(div_location)
             self.clean_up_brand_name(div_location)
-
 
 
 kelloggs_wiki = KelloggsWikiScraper()
