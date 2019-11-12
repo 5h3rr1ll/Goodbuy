ed2d4b1fe38fe88caafa6c2a982cd48f2811997f
Anthony <anthony.sherrill@code.berlin>
2019-11-12 21:14:14 +0100

    refactored brands lookup

--- scraper/mars_brands.py
+++ scraper/mars_brands.py
@@ -1,7 +1,8 @@
+import re
+
 import requests
 from bs4 import BeautifulSoup
 
-import re
 
 class MarsWikiScraper:
     def __init__(self):
@@ -18,28 +19,38 @@ class MarsWikiScraper:
             "http://localhost:8000/goodbuyDatabase/save_brand/", json=data,
         )
 
+    def clean_up_brand_name(self, bs_object):
+        try:
+            if bs_object.findAll("li") == []:
+                print("empty Error")
+            else:
+                for list_element in bs_object.findAll("li"):
+                    link_text = list_element.get_text()
+                    special_char = re.findall("[\][–)(,}:]|[0-9]{4}", link_text)
+                    try:
+                        print(link_text.split(special_char[0])[0])
+                        self.save_brand(link_text.split(special_char[0])[0].strip())
+                    except:
+                        print(link_text)
+                        self.save_brand(link_text.strip())
+        except AttributeError as e:
+            print(str(e), " div changed position ")
 
-    def get_rid_of(self, bs_object):
-        for list_element in bs_object.findAll("li"):
-            link_text = list_element.get_text()
-            special_char = re.findall("[\][–)(,}:]|[0-9]{4}", link_text)
-            try:
-                print(link_text.split(special_char[0])[0])
-                self.save_brand(link_text.split(special_char[0])[0].strip())
-            except:
-                print(link_text)
-                self.save_brand(link_text.strip())
-
-    def get_all_categories(self):
-        list_of_div_locations = {
+    def get_all_div_location(self):
+        div_locations_list = {
             "Original products": "#mw-content-text > div > div:nth-child(68)",
             "Products manufactured by The Wrigley Company": "#mw-content-text > div > div:nth-child(70)",
             "Products for pet consumption": "#mw-content-text > div > div:nth-child(72)",
         }
-        for category, div_location in list_of_div_locations.items():
+        return div_locations_list
+
+    def iterate_over_list(self, lst):
+        for category, div_location in lst.items():
             print(f"\nCategory: {category}")
-            self.get_rid_of(self.soup.select_one(div_location))
+            div_location = self.soup.select_one(div_location)
+            self.clean_up_brand_name(div_location)
 
 
 mars_wiki = MarsWikiScraper()
-mars_wiki.get_all_categories()
+div_locations_list = mars_wiki.get_all_div_location()
+mars_wiki.iterate_over_list(div_locations_list)
--- scraper/mondelez(kraft)_brands.py
+++ scraper/mondelez(kraft)_brands.py
@@ -1,7 +1,7 @@
+import re
+
 import requests
 from bs4 import BeautifulSoup
-
-import re
 
 class MondelezWikiScraper:
     def __init__(self):
@@ -18,26 +18,36 @@ class MondelezWikiScraper:
             "http://localhost:8000/goodbuyDatabase/save_brand/", json=data,
         )
 
+    def clean_up_brand_name(self, bs_object):
+        try:
+            if bs_object.findAll("li") == []:
+                print("empty Error")
+            else:
+                for list_element in bs_object.findAll("li"):
+                    link_text = list_element.get_text()
+                    special_char = re.findall("[\][–)(,}:]|[0-9]{4}", link_text)
+                    try:
+                        print(link_text.split(special_char[0])[0])
+                        self.save_brand(link_text.split(special_char[0])[0].strip())
+                    except:
+                        print(link_text)
+                        self.save_brand(link_text.strip())
+        except AttributeError as e:
+            print(str(e), " div changed position ")
 
-    def get_rid_of(self, bs_object):
-        for list_element in bs_object.findAll("li"):
-            link_text = list_element.get_text()
-            special_char = re.findall("[\][–)(,}:]|[0-9]{4}", link_text)
-            try:
-                print(link_text.split(special_char[0])[0])
-                self.save_brand(link_text.split(special_char[0])[0].strip())
-            except:
-                print(link_text)
-                self.save_brand(link_text.strip())
-
-    def get_all_categories(self):
-        list_of_div_locations = {
-            "Current brands" : "#mw-content-text > div > div.div-col.columns.column-width",
+    def get_all_div_location(self):
+        div_locations_list = {
+            "Current brands": "#mw-content-text > div > div.div-col.columns.column-width",
         }
-        for category, div_location in list_of_div_locations.items():
-            print(f"\nCategory: {category}")
-            self.get_rid_of(self.soup.select_one(div_location))
+        return div_locations_list
+
+    def iterate_over_list(self, lst):
+        for category, div_location in lst.items():
+            print(f"\n{category}:")
+            div_location = self.soup.select_one(div_location)
+            self.clean_up_brand_name(div_location)
 
 
 mondelez_wiki = MondelezWikiScraper()
-mondelez_wiki.get_all_categories()
+div_locations_list = mondelez_wiki.get_all_div_location()
+mondelez_wiki.iterate_over_list(div_locations_list)
