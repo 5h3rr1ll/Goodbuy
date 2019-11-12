60118dcb9e9176e044577254d3a89828659901ff
Anthony <anthony.sherrill@code.berlin>
2019-11-12 22:04:59 +0100

    Adapted kellogs lookup to PEP8 due to pylint

--- scraper/kelloggs_brands.py
+++ scraper/kelloggs_brands.py
@@ -1,3 +1,4 @@
+"""This module gathers information about brands and companies of corperations."""
 import re
 
 import requests
@@ -12,7 +13,9 @@ class KelloggsWikiScraper:
         self.request = requests.get(self.url)
         self.soup = BeautifulSoup(self.request.content, "html.parser")
 
-    def save_brand(self, brand):
+    @classmethod
+    def save_brand(cls, brand):
+        """Takes a brand name and saves it with the corporation into the database"""
         data = {
             "name": brand,
             "corporation": "Kellogg's",
@@ -22,23 +25,33 @@ class KelloggsWikiScraper:
         )
 
     def clean_up_brand_name(self, bs_object):
+        """
+        Takes a BeautifulSoup object, searches for all links in it, interate through it. searches
+        for special characters to remove unneeded text from the link text. In the end the name
+        gets handed over to the save_brand function.
+        """
         try:
             if bs_object.findAll("li") == []:
                 print("empty Error")
             else:
                 for list_element in bs_object.findAll("li"):
                     link_text = list_element.get_text()
-                    special_char = re.findall("[\][–)(,}:]|[0-9]{4}", link_text)
+                    special_char = re.findall(r"[\][–)(,}:]|[0-9]{4}", link_text)
                     try:
                         print(link_text.split(special_char[0])[0])
                         self.save_brand(link_text.split(special_char[0])[0].strip())
-                    except:
+                    except IndexError:
                         print(link_text)
                         self.save_brand(link_text.strip())
-        except AttributeError as e:
-            print(str(e), " div changed position ")
+        except AttributeError:
+            print(" div changed position", str(AttributeError))
 
-    def get_all_div_location(self):
+    @classmethod
+    def get_all_div_location(cls):
+        """
+        Returns a dictionary with locations of the brand list, located by css selctor, combind with
+        with the sections title.
+        """
         div_locations_list = {
             "Products": "#mw-content-text > div > div:nth-child(26)",
             "Cereal": "#mw-content-text > div > div:nth-child(30)",
@@ -46,12 +59,14 @@ class KelloggsWikiScraper:
         return div_locations_list
 
     def iterate_over_list(self, lst):
+        """
+        Iterates over the list of div locations. Finds the div and call clean_up_brand_name on it.
+        """
         for category, div_location in lst.items():
             print(f"\n{category}:")
             div_location = self.soup.select_one(div_location)
             self.clean_up_brand_name(div_location)
 
 
-kelloggs_wiki = KelloggsWikiScraper()
-div_locations_list = kelloggs_wiki.get_all_div_location()
-kelloggs_wiki.iterate_over_list(div_locations_list)
+KELLOGS_WIKI = KelloggsWikiScraper()
+KELLOGS_WIKI.iterate_over_list(KELLOGS_WIKI.get_all_div_location())
