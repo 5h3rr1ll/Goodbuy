889be4c295bf69e17046eed73013bf13e160cb2a
Anthony <anthony.sherrill@code.berlin>
2019-11-12 11:24:10 +0100

    refactored scraper code

--- scraper/kelloggs_brands.py
+++ scraper/kelloggs_brands.py
@@ -19,26 +19,39 @@ class KelloggsWikiScraper:
             "http://localhost:8000/goodbuyDatabase/save_brand/", json=data,
         )
 
-    def get_rid_of(self, bs_object):
-        for list_element in bs_object.findAll("li"):
-            link_text = list_element.get_text()
-            special_char = re.findall("[\][–)(,}:]|[0-9]{4}", link_text)
-            try:
-                print(link_text.split(special_char[0])[0])
-                self.save_brand(link_text.split(special_char[0])[0].strip())
-            except:
-                print(link_text)
-                self.save_brand(link_text.strip())
+    def clean_up_brand_name(self, bs_object):
+        try:
+            if bs_object.findAll("li") == []:
+                print('empty Error')
+            else:
+                for list_element in bs_object.findAll("li"):
+                    link_text = list_element.get_text()
+                    special_char = re.findall("[\][–)(,}:]|[0-9]{4}", link_text)
+                    try:
+                        print(link_text.split(special_char[0])[0])
+                        self.save_brand(link_text.split(special_char[0])[0].strip())
+                    except:
+                        print(link_text)
+                        self.save_brand(link_text.strip())
+        except AttributeError as e:
+            print(str(e), ' div changed position ')
 
-    def get_all_products(self):
-        list_of_div_locations = {
+
+    def get_all_div_location(self):
+        div_locations_list = {
             "Products" : "#mw-content-text > div > div:nth-child(26)",
             "Cereal" : "#mw-content-text > div > div:nth-child(30)",
         }
-        for Category, div_location in list_of_div_locations.items():
+        return div_locations_list
+
+    def iterate_over_list(self, lst):
+        for Category, div_location in lst.items():
             print(f"\nBrands starting with: {Category}")
-            self.get_rid_of(self.soup.select_one(div_location))
+            div_location = self.soup.select_one(div_location)
+            self.clean_up_brand_name(div_location)
+
 
 
 kelloggs_wiki = KelloggsWikiScraper()
-kelloggs_wiki.get_all_products()
+div_locations_list = kelloggs_wiki.get_all_div_location()
+kelloggs_wiki.iterate_over_list(div_locations_list)
