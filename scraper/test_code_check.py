af4853568d5ae9f36340255ec94df764ab4d27c6
Darjusch <Darjusch.schrand@code.berlin>
2019-11-13 10:10:32 +0100

    working codecheck tests

--- scraper/test_code_check.py
+++ scraper/test_code_check.py
@@ -1,14 +1,15 @@
 from django.test import TestCase
-import requests, json
+import requests, json, ast
 
 class CodeCheck(TestCase):
 
-    def setUp(self):
-        pass
+    def test_if_code_check_is_still_running(self):
+        response = requests.get('https://www.codecheck.info/')
+        self.assertEqual(response.status_code, 200)
 
     def test_if_code_check_returns_right_product(self):
         result = requests.get('http://localhost:8000/lookup/42113539')
-        expected_product = str({"code": "42113539", "name": "Big Red (Wrigleys)", "brand": "Big Red", "product_category": "Kaugummi & Kaudragees", "scraped_image": "https://cdn.codecheck.info/image/prod/0018/6172/186172_regular.jpg"})
-        print(result.text)
-        self.assertEqual(result.status_code, 200)
-        self.assertEqual(str(result.text), expected_product)
+        expected_product = {"code": "42113539", "name": "Big Red (Wrigley\u00b4s)", "brand": "Big Red", "product_category": "Kaugummi & Kaudragees", "scraped_image": "https://cdn.codecheck.info/image/prod/0018/6172/186172_regular.jpg"}
+        result_as_dict = ast.literal_eval(result.text)
+        self.assertEqual(result_as_dict, expected_product)
+
