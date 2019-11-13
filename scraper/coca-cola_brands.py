d4fb53c041f84f1e17c75789ff42547fbd1c47ed
Anthony <anthony.sherrill@code.berlin>
2019-11-14 00:22:28 +0100

    improved coca-cola lookup file

--- scraper/coca-cola_brands.py
+++ scraper/coca-cola_brands.py
@@ -1,3 +1,4 @@
+"""This module gathers information about brands and companies of corperations."""
 import re
 
 import requests
@@ -5,33 +6,42 @@ from bs4 import BeautifulSoup
 
 
 class CocaColaWikiScraper:
+    """Returns brands of Coca-Cola"""
+
     def __init__(self):
         self.url = "https://en.wikipedia.org/wiki/List_of_Coca-Cola_brands"
         self.request = requests.get(self.url)
         self.soup = BeautifulSoup(self.request.content, "html.parser")
 
-    def save_brand(self, brand):
+    @classmethod
+    def save_brand(cls, brand):
+        """Takes a brand name and saves it with the corporation into the database"""
         data = {
             "name": brand,
-            "corporation": "Coca-Cola",
+            "corporation": "Kellogg's",
         }
         requests.post(
             "http://localhost:8000/goodbuyDatabase/save_brand/", json=data,
         )
 
     def clean_up_brand_name(self, bs_object):
+        """
+        Takes a BeautifulSoup object, searches for all links in it, interate through it. searches
+        for special characters to remove unneeded text from the link text. In the end the name
+        gets handed over to the save_brand function.
+        """
         try:
             if bs_object.findAll("li") == []:
                 print("empty Error")
             else:
                 for list_element in bs_object.findAll("li"):
                     link_text = list_element.get_text()
-                    special_char = re.findall("[\][–)(,}:]|[0-9]{4}", link_text)
+                    special_char = re.findall(r"[\][–)(,}:]|[0-9]{4}", link_text)
                     try:
                         print(link_text.split(special_char[0])[0])
                         self.save_brand(link_text.split(special_char[0])[0].strip())
-                    except:
-                        print(link_text)
+                    except Exception:
+                        print(str(Exception), link_text)
                         self.save_brand(link_text.strip())
         except AttributeError as e:
             print(str(e), " div changed position ")
@@ -67,12 +77,14 @@ class CocaColaWikiScraper:
         return div_locations_list
 
     def iterate_over_list(self, lst):
+        """
+        Iterates over the list of div locations. Finds the div and call clean_up_brand_name on it.
+        """
         for category, div_location in lst.items():
-            print(f"\n{Category}:")
+            print(f"\n{category}:")
             div_location = self.soup.select_one(div_location)
             self.clean_up_brand_name(div_location)
 
 
-coca_wiki = CocaColaWikiScraper()
-div_locations_list = coca_wiki.get_all_div_location()
-coca_wiki.iterate_over_list(div_locations_list)
+COCA_WIKI = CocaColaWikiScraper()
+COCA_WIKI.iterate_over_list(COCA_WIKI.get_all_div_location())
