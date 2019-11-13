3ae595425b9f63d60bb0960bd155337b2b94bd81
Anthony <anthony.sherrill@code.berlin>
2019-11-13 23:02:08 +0100

    formated bue to PEP8

--- goodbuyDatabase/admin.py
+++ goodbuyDatabase/admin.py
@@ -1,19 +1,38 @@
 from django.contrib import admin
-from .models import (
-    Corporation, Company, Brand,
-    Product, Country, Rating,
-    Store, ProductPriceInStore,
-    Certificate, CategoryOfProduct
-    )
+
+from .models import (Brand, CategoryOfProduct, Certificate, Company,
+                     Corporation, Country, Product, ProductPriceInStore,
+                     Rating, Store)
+
+
 # Register your models here.
 class CategoryOfProductAdmin(admin.ModelAdmin):
-    list_display = ("id", "name", "created", "updated",)
-    list_display_links = ("id", "name", "created", "updated",)
+    list_display = (
+        "id",
+        "name",
+        "created",
+        "updated",
+    )
+    list_display_links = (
+        "id",
+        "name",
+        "created",
+        "updated",
+    )
+
 
 class RatingAdmin(admin.ModelAdmin):
-    list_display = ("id", "corporation" , "humans", "environment", "animals",
-     "animals_description", "environment_description", "humans_description")
-    list_display_links = ("id", "corporation",  "humans", "environment", "animals")
+    list_display = (
+        "id",
+        "corporation",
+        "humans",
+        "environment",
+        "animals",
+        "animals_description",
+        "environment_description",
+        "humans_description",
+    )
+    list_display_links = ("id", "corporation", "humans", "environment", "animals")
 
     @classmethod
     def corporation(self, obj):
@@ -21,27 +40,56 @@ class RatingAdmin(admin.ModelAdmin):
             return obj.corporation.name
         else:
             return None
+
     corporation.short_description = "Rating of associated Concern"
 
+
 class CountryAdmin(admin.ModelAdmin):
-    list_display = ("id", "name", "code", "created", "updated",)
-    list_display_links = ("id", "name",)
-    search_fields = ["id", "name", "code", "created", "updated",]
+    list_display = (
+        "id",
+        "name",
+        "code",
+        "created",
+        "updated",
+    )
+    list_display_links = (
+        "id",
+        "name",
+    )
+    search_fields = [
+        "id",
+        "name",
+        "code",
+        "created",
+        "updated",
+    ]
+
 
 class RatingInline(admin.StackedInline):
     max_num = 1
     model = Rating
 
+
 class CorporationAdmin(admin.ModelAdmin):
     list_display = (
-        "id", "name", "logo", "wiki",
-        "origin_code", "created", "updated",
-        )
+        "id",
+        "name",
+        "logo",
+        "wiki",
+        "origin_code",
+        "created",
+        "updated",
+    )
     list_display_links = ("id", "name")
     search_fields = [
-        "id", "name", "logo", "wiki",
-        "origin_code", "created", "updated",
-        ]
+        "id",
+        "name",
+        "logo",
+        "wiki",
+        "origin_code",
+        "created",
+        "updated",
+    ]
     autocomplete_fields = ("origin",)
     inlines = [
         RatingInline,
@@ -53,16 +101,28 @@ class CorporationAdmin(admin.ModelAdmin):
         else:
             return None
 
+
 class CompanyAdmin(admin.ModelAdmin):
     list_display = (
-        "id", "name", "logo", "wiki", "corporation",
-        "origin_code", "created", "updated",
-        )
+        "id",
+        "name",
+        "logo",
+        "wiki",
+        "corporation",
+        "origin_code",
+        "created",
+        "updated",
+    )
     list_display_links = ("id", "name")
     search_fields = [
-        "id", "name", "logo", "corporation__name",
-        "origin_code", "created", "updated",
-        ]
+        "id",
+        "name",
+        "logo",
+        "corporation__name",
+        "origin_code",
+        "created",
+        "updated",
+    ]
     autocomplete_fields = ("corporation",)
 
     def origin_code(self, obj):
@@ -71,65 +131,147 @@ class CompanyAdmin(admin.ModelAdmin):
         else:
             return None
 
+
 class BrandAdmin(admin.ModelAdmin):
     list_display = (
-        "id", "name", "logo", "wiki", "company",
-        "corporation", "created", "updated",
-        )
+        "id",
+        "name",
+        "logo",
+        "wiki",
+        "company",
+        "corporation",
+        "created",
+        "updated",
+    )
     list_display_links = ("id", "name")
     search_fields = [
-        "id", "name", "logo", "wiki", "company__name",
-        "corporation__name", "created", "updated",
-        ]
-    autocomplete_fields = ("corporation", "company",)
+        "id",
+        "name",
+        "logo",
+        "wiki",
+        "company__name",
+        "corporation__name",
+        "created",
+        "updated",
+    ]
+    autocomplete_fields = (
+        "corporation",
+        "company",
+    )
+
 
 class ProdcutPriceInStoreAdmin(admin.ModelAdmin):
-    list_display = ("id","store","product","price")
-    search_fields = [
-        "id","store__name","product__name","price"
-    ]
+    list_display = ("id", "store", "product", "price")
+    search_fields = ["id", "store__name", "product__name", "price"]
+
 
 class PriceInline(admin.StackedInline):
     max_num = 1
     model = ProductPriceInStore
 
+
 class CertificateAdmin(admin.ModelAdmin):
-    list_display = ("id", "name", "wiki", "created", "updated",)
+    list_display = (
+        "id",
+        "name",
+        "wiki",
+        "created",
+        "updated",
+    )
     list_display_links = ("id", "name")
-    search_fields = ["id", "name", "created", "updated",]
+    search_fields = [
+        "id",
+        "name",
+        "created",
+        "updated",
+    ]
     autocomplete_fields = ("product",)
+
 
 class ProductAdmin(admin.ModelAdmin):
     list_display = (
-        "id", "name","added_by","checked","checked_by","logo", "wiki", "code",
-        "scraped_image", "brand", "corporation",
-        "scanned_counter" ,"created", "updated",
-        )
+        "id",
+        "name",
+        "added_by",
+        "checked",
+        "checked_by",
+        "logo",
+        "wiki",
+        "code",
+        "scraped_image",
+        "brand",
+        "corporation",
+        "scanned_counter",
+        "created",
+        "updated",
+    )
     list_display_links = ("id", "name")
     search_fields = [
-        "id", "name", "logo", "wiki", "code",
-        "image", "brand__name", "corporation__name",
-        "scanned_counter" ,"created", "updated", "checked_by__name",
-        ]
-    exclude = ("scanned_counter","added_by")
-    autocomplete_fields = ("brand", "corporation",)
-    inlines = [ PriceInline, ]
+        "id",
+        "name",
+        "logo",
+        "wiki",
+        "code",
+        "scraped_image",
+        "brand__name",
+        "corporation__name",
+        "scanned_counter",
+        "created",
+        "updated",
+        "checked_by__name",
+    ]
+    exclude = ("scanned_counter", "added_by")
+    autocomplete_fields = (
+        "brand",
+        "corporation",
+    )
+    inlines = [
+        PriceInline,
+    ]
+
 
 class StoreAdmin(admin.ModelAdmin):
-    list_display = ("id", "name", "country",)
+    list_display = (
+        "id",
+        "name",
+        "country",
+    )
+    list_display_links = ("id", "name")
     search_fields = [
-        "id", "name",
+        "id",
+        "name",
     ]
     autocomplete_fields = ()
 
+
 admin.site.site_header = "Goodbuy Database"
-admin.site.register(Corporation, CorporationAdmin,)
-admin.site.register(Company, CompanyAdmin,)
-admin.site.register(Brand, BrandAdmin,)
-admin.site.register(Product, ProductAdmin,)
-admin.site.register(Country, CountryAdmin,)
-admin.site.register(Rating, RatingAdmin,)
-admin.site.register(CategoryOfProduct, CategoryOfProductAdmin,)
-admin.site.register(Store, StoreAdmin,)
-admin.site.register(ProductPriceInStore, ProdcutPriceInStoreAdmin,)
-admin.site.register(Certificate,CertificateAdmin,)
+admin.site.register(
+    Corporation, CorporationAdmin,
+)
+admin.site.register(
+    Company, CompanyAdmin,
+)
+admin.site.register(
+    Brand, BrandAdmin,
+)
+admin.site.register(
+    Product, ProductAdmin,
+)
+admin.site.register(
+    Country, CountryAdmin,
+)
+admin.site.register(
+    Rating, RatingAdmin,
+)
+admin.site.register(
+    CategoryOfProduct, CategoryOfProductAdmin,
+)
+admin.site.register(
+    Store, StoreAdmin,
+)
+admin.site.register(
+    ProductPriceInStore, ProdcutPriceInStoreAdmin,
+)
+admin.site.register(
+    Certificate, CertificateAdmin,
+)
