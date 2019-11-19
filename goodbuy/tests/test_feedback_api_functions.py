3698852e1cf26d8890d9802b4939644ac762548c
Darjusch <Darjusch.schrand@code.berlin>
2019-11-19 15:43:57 +0100

    updated tests after refactoring

--- goodbuy/tests/test_feedback_api_functions.py
+++ goodbuy/tests/test_feedback_api_functions.py
@@ -1,8 +1,6 @@
 from unittest import TestCase
 import requests
-
-from goodbuyDatabase.models import Brand, Corporation
-
+from goodbuyDatabase.models import Corporation, Product
 
 
 class TestFeedbackApiFunctions(TestCase):
@@ -11,24 +9,33 @@ class TestFeedbackApiFunctions(TestCase):
         self.ean_code_in_db = '2000423339488'
         self.ean_code_not_in_db = '4000582185399'
 
-        self.corporation_obj = Corporation.objects.create(
-            name='Coke'
+        self.corporation_obj = Corporation.objects.get_or_create(
+            name='Coca-Cola'
         )
-        self.brand_obj = Brand.objects.create(
-            name='Coca-Cola',
-            corporation='Coke'
+        self.corporation_obj_not_in_big_ten = Corporation.objects.get_or_create(
+            name='Twix'
         )
+        self.product_obj = Product.objects.get_or_create(
+            code=self.ean_code_in_db,
+            corporation=Corporation.objects.get(name='Coca-Cola')
+        )
+        self.product_obj_not_in_big_ten = Product.objects.get_or_create(
+            code=self.ean_code_not_in_db,
+            corporation=Corporation.objects.get(name='Twix')
+        )
+    def test_corporation_of_product_is_in_big_ten(self):
+        response = requests.get(f'http://127.0.0.1:8000/is_big_ten/{self.ean_code_in_db}/')
+        print(response.status_code)
+        self.assertEqual(response.text, 'True')
 
-    def test_corporation_of_brand_is_in_big_ten(self):
-        response = requests.get('http://127.0.0.1:8000/is_big_ten/Coca%20Cola/')
-        self.assertEqual(response.text, True)
 
-
-    def test_corporation_of_brand_is_not_in_database(self):
-        response = requests.get('http://127.0.0.1:8000/is_big_ten/Snickers/')
+    def test_corporation_of_product_is_not_in_database(self):
+        response = requests.get(f'http://127.0.0.1:8000/is_big_ten/1984781927381/')
+        print(response.status_code)
         self.assertEqual(response.text, 'We don\'t know')
 
 
-    def test_corportation_of_brand_is_not_in_big_ten(self):
-        response = requests.get('http://127.0.0.1:8000/is_big_ten/Schnick/')
-        self.assertEqual(response.text, False)
+    def test_corportation_of_product_is_not_in_big_ten(self):
+        response = requests.get(f'http://127.0.0.1:8000/is_big_ten/{self.ean_code_not_in_db}/')
+        print(response.status_code)
+        self.assertEqual(response.text, 'False')
