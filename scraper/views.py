936580c922a9809a76ae3f346003fb33d3732dbc
Anthony <anthony.sherrill@code.berlin>
2019-11-19 21:16:51 +0100

    Added some strings to see better the progress in the terminal

--- scraper/views.py
+++ scraper/views.py
@@ -1,7 +1,7 @@
 import os
 
 import requests
-from django.http import HttpResponse, JsonResponse
+from django.http import HttpResponse
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
@@ -24,26 +24,28 @@ def scrape(request, code):
     driver.set_window_size(1200, 1134)
     driver.get("https://codecheck.info")
 
-    print("\n search field")
+    print("\nSearch for search field...")
     try:
         search_field = WebDriverWait(driver, 10).until(
             EC.presence_of_element_located((By.ID, "search-query"))
         )
         search_field.clear()
         search_field.send_keys(f"{code}")
+        print(" Search found.")
     except Exception as e:
-        print("\nsearch field ERROR:", str(e))
+        print("  Search field ERROR:", str(e))
 
-    print("\n search button")
+    print("\nSearch for search button...")
     try:
         search_button = WebDriverWait(driver, 10).until(
             EC.presence_of_element_located((By.ID, "search-submit"))
         )
         search_button.click()
+        print(" Button clicked.")
     except Exception as e:
-        print("\n Search submit button ERROR:", str(e))
+        print("  Search submit button ERROR:", str(e))
 
-    print("\n product name")
+    print("\nSearch for product name...")
     try:
         product_name = (
             WebDriverWait(driver, 10)
@@ -54,22 +56,24 @@ def scrape(request, code):
             )
             .text
         )
+        print(f" Product name is {product_name}.")
     except Exception as e:
         # TODO: if name is not found, user needs to get redirected to add product form
-        print("\n Productname ERROR:", str(e))
+        print("  Productname ERROR:", str(e))
 
-    print("\n product image")
+    print("\nSearch for product image...")
     try:
         product_image = (
             WebDriverWait(driver, 10)
             .until(EC.presence_of_element_located((By.CSS_SELECTOR, ".nf > img")))
             .get_attribute("src")
         )
+        print(f" Product image found at {product_image}.")
     except Exception as e:
-        print("\n Product image ERROR:", str(e))
+        print("  Product image ERROR:", str(e))
 
     product_category = None
-    print("\n product category")
+    print("\nSearch for product category...")
     try:
         product_category = (
             WebDriverWait(driver, 10)
@@ -83,10 +87,11 @@ def scrape(request, code):
             )
             .text
         )
+        print(f" Product category is {product_category}.")
     except Exception as e:
-        print("\n Product category ERROR:", str(e))
+        print("  Product category ERROR:", str(e))
 
-    print("\n more product details div")
+    print("\nFind more product details div")
     try:
         more_product_detail_div = WebDriverWait(driver, 10).until(
             EC.presence_of_element_located(
@@ -95,28 +100,25 @@ def scrape(request, code):
         )
         more_product_detail_div.click()
     except Exception as e:
-        print("\n More Product Details Div not found:", str(e))
+        print("  More Product Details Div not found:", str(e))
 
-    print("\n interate over product info items")
+    print("\nInterate over product info items to find product brand...")
 
     product_brand = None
     try:
         product_info_items = driver.find_elements_by_class_name("product-info-item")
         for div in product_info_items:
             print("\n Text:", div.text)
-            if (
-                div.text.splitlines()[0] == "Marke"
-                or div.text.splitlines()[0] == "Marke"
-            ):
+            if div.text.splitlines()[0] == "Marke":
                 product_brand = div.text.splitlines()[1]
+        print(f" Product brand is {product_brand}.")
     except Exception as e:
-        print("\n Can't extract brand:", str(e))
+        print("  Can't extract brand:", str(e))
         # When there is no brand in the scraped object we return the Httpresponse(403).
         # The Api should know that this is a response to redirect the user in Vue to a view for
         # inserting the brand.
         return HttpResponse(403)
 
-    print("Product is done")
     product = {
         "code": code,
         "name": product_name,
@@ -124,8 +126,8 @@ def scrape(request, code):
         "product_category": product_category,
         "scraped_image": product_image,
     }
-    print("Looked up product: ", product)
+    print("✅ Looked up product: ", product)
     requests.post(
         f"{os.environ.get('CURRENT_HOST')}/goodbuyDatabase/save_product/", json=product,
     )
-    return JsonResponse(product)
+    return str(product)
