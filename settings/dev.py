0d8912431f96480a58afa040e74136b02cfe8503
Anthony <anthony.sherrill@code.berlin>
2019-11-28 15:47:00 +0100

    new dev-database credentials

--- settings/dev.py
+++ settings/dev.py
@@ -3,10 +3,10 @@ from .base import *
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'USER': "twwvczmzjozniv",
-        'NAME': "d7idpuukb69euf",
-        'PASSWORD': "f1f4c7f771817dfde419164fd6a596bcf443a1e711a8052e666b61c2f39e1180",
-        'HOST': "ec2-54-217-228-25.eu-west-1.compute.amazonaws.com",
+        'USER': "qrmvvwygxawxei",
+        'NAME': "dav2jjcqirbg54",
+        'PASSWORD': "ccbd108723da7f5be903ade4333353a40c5ddcda4d286358c91ee5e3f209f105",
+        'HOST': "ec2-54-217-243-19.eu-west-1.compute.amazonaws.com",
         'PORT': '5432',
         }
     }
