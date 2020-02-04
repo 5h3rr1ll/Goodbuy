b0d53c3ddeefa4b97e98fe7935846a3be5d32e68
Anthony <anthony.sherrill@code.berlin>
2020-02-04 23:59:00 +0100

    kein plan warum ich das schon wieder committen muss 😀

--- scraper/views.py
+++ scraper/views.py
@@ -1,6 +1,6 @@
 import os
 
-import requests
+from django.core import serializers
 from django.http import HttpResponse
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
@@ -8,29 +8,26 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 
+from goodbuyDatabase.models import (
+    Brand,
+    MainProductCategory,
+    Product,
+    ProductCategory,
+    SubProductCategory,
+)
+
 
 def scrape(code):
-    print(f"Start scraper for {code}")
-    print("Intial product save.")
-    product = {
-        "code": code,
-        "name": None,
-        "brand": None,
-        "main_product_category": None,
-        "product_category": None,
-        "sub_product_category": None,
-        "scraped_image": None,
-        "state": "209",
-        "data_source": "2",
-    }
-    requests.get(
-        f"{os.environ.get('CURRENT_HOST')}/goodbuyDatabase/save_product/", json=product,
-    )
+    print(f"Starting scraper for {code}")
+    product = Product(code=code, state="209")
+    product.save()
+
     options = Options()
     options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
     options.add_argument("--headless")
     options.add_argument("--disable-gpu")
     options.add_argument("--no-sandbox")
+    options.add_argument("user-agent=Anon")
     prefs = {"profile.managed_default_content_settings.images": 2}
     options.add_experimental_option("prefs", prefs)
     driver = webdriver.Chrome(
@@ -47,7 +44,7 @@ def scrape(code):
         )
         search_field.clear()
         search_field.send_keys(f"{code}")
-        print(" Search found.")
+        print(" Search field found.")
     except Exception as e:
         print("  Search field ERROR:", str(e))
 
@@ -67,23 +64,51 @@ def scrape(code):
             EC.presence_of_element_located((By.CLASS_NAME, "bc"))
         )
         spans = div.find_elements_by_class_name("bcd")
-        breadcrumbs = []
-        for breadcrumb in spans:
-            breadcrumbs.append(breadcrumb.text)
-        product["main_product_category"] = breadcrumbs[1]
-        product["product_category"] = breadcrumbs[2]
-        product["sub_product_category"] = breadcrumbs[-1]
-        breadcrumb_string = div.text.split(f"{breadcrumbs[-2] + ' ' + breadcrumbs[-1]}")
-        product["name"] = breadcrumb_string[-1].strip()
-        print(
-            f"""
-            Product name: {product['name']}
-            Product Category: {product['product_category']}
-            Sub-Product Category: {product['sub_product_category']}
-            """
-        )
+
+        if len(spans) >= 3:
+            breadcrumbs = []
+            for breadcrumb in spans:
+                breadcrumbs.append(breadcrumb.text)
+            product.main_product_category = MainProductCategory.objects.get_or_create(
+                name=breadcrumbs[1]
+            )[0]
+            product.product_category = ProductCategory.objects.get_or_create(
+                name=breadcrumbs[2]
+            )[0]
+            product.sub_product_category = SubProductCategory.objects.get_or_create(
+                name=breadcrumbs[-1]
+            )[0]
+            breadcrumb_string = div.text.split(
+                f"{breadcrumbs[-2] + ' ' + breadcrumbs[-1]}"
+            )
+            product.name = breadcrumb_string[-1].strip()
+            print(
+                f"""
+                Product name: {product.name}
+                Product Category: {product.product_category}
+                Sub-Product Category: {product.sub_product_category}
+                """
+            )
+        elif len(spans) == 1:
+            product.name = (
+                WebDriverWait(driver, 10)
+                .until(
+                    EC.presence_of_element_located((By.XPATH, '//*[@id="t-1263277"]'))
+                )
+                .text
+            )
+            print(f"  Product name: {product.name}")
+        else:
+            print("\nStill something wrong here!\n")
     except Exception as e:
         print(str(e))
+        product.state = "306"
+        product_obj_as_json = serializers.serialize(
+            "json", Product.objects.filter(code=code)
+        )
+        return HttpResponse(
+            product_obj_as_json, content_type="text/json-comment-filtered"
+        )
 
     print("\nSearch for product image...")
     try:
@@ -91,11 +116,9 @@ def scrape(code):
             no_image = WebDriverWait(driver, 10).until(
                 EC.presence_of_element_located((By.CSS_SELECTOR, ".no-image"))
             )
-
-            print("no-image but doesnt throw error")
-
+            print("no-image but doesn't throw error")
         except Exception:
-            product["scraped_image"] = (
+            product.scraped_image = (
                 WebDriverWait(driver, 10)
                 .until(EC.presence_of_element_located((By.CSS_SELECTOR, ".nf > img")))
                 .get_attribute("src")
@@ -126,37 +149,39 @@ def scrape(code):
         print("  More Product Details Div not found:", str(e))
 
     print("\nInterate over product info items to find product brand...")
-
     try:
         product_info_items = driver.find_elements_by_class_name("product-info-item")
+
         for div in product_info_items:
             print("\n Text:", div.text)
             if div.text.splitlines()[0] == "Marke":
-                product["brand"] = div.text.splitlines()[1]
-        print(f" Product brand is {product['brand']}.")
+                print(
+                    f"\nBrand Print: {Brand.objects.get_or_create(name=div.text.splitlines()[1])[0]}\n"
+                )
+                product.brand = Brand.objects.get_or_create(
+                    name=div.text.splitlines()[1]
+                )[0]
+        print(f" Product brand is {product.brand}.")
     except Exception as e:
-        product["state"] = "306"
-        requests.post(
-            f"{os.environ.get('CURRENT_HOST')}/goodbuyDatabase/save_product/",
-            json=product,
+        product.state = "306"
+        print(
+            "  Can't extract brand:",
+            str(e),
+            Brand.objects.get_or_create(name=div.text.splitlines()[1])[0],
         )
-        print("  Can't extract brand:", str(e))
         return HttpResponse(status=306)
-    print(f'\nName: {product["name"]} Brand: {product["brand"]}\n')
-    if product["name"] is not None and product["brand"] is not None:
-        product["state"] = "200"
+    print(f"\nName: {product.name} Brand: {product.brand}\n")
+
+    if product.name is not None and product.brand is not None:
+        product.state = "200"
+        product.data_source = "2"
         print("✅ Looked up product: ", product)
-        requests.post(
-            f"{os.environ.get('CURRENT_HOST')}/goodbuyDatabase/save_product/",
-            json=product,
-        )
     else:
-        product["state"] = "306"
-        product["scraped_image"] = None
-        requests.post(
-            f"https://dev-goodbuy.herokuapp.com/goodbuyDatabase/save_product/",
-            json=product,
-        )
+        product.state = "306"
         print("Name and/or brand is None")
+    product.save()
     driver.quit()
-    return str(product)
+    product_obj_as_json = serializers.serialize(
+        "json", Product.objects.filter(code=code)
+    )
+    return HttpResponse(product_obj_as_json, content_type="text/json-comment-filtered")
