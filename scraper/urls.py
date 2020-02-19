89bc82e47f11e186e0f544bc3a534151db1b62c5
Anthony <anthony.sherrill@code.berlin>
2020-02-19 08:13:17 +0100

    adapted url.py to have seperated pathes for scrape locally or on lambda

--- scraper/urls.py
+++ scraper/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from scraper.views import scrape
+from scraper.django_cc_crawler import scrape as local_scrape
 
 app_name = "scraper"
 
 urlpatterns = [
-    path("<str:code>/", scrape, name="scrape"),
+    path("<str:code>/locally", local_scrape, name="local_scrape"),
+    # path("<str:code>/", aws_lambda_scrape, name="aws_lambda_scrape"),
 ]
