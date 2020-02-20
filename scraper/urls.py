53f886235c925f3661a48aae291e66ce35426da1
Anthony <anthony.sherrill@code.berlin>
2020-02-20 10:13:40 +0100

    cleared scraper urls.py

--- scraper/urls.py
+++ scraper/urls.py
@@ -1,9 +1,7 @@
 from django.urls import path
-from scraper.django_cc_crawler import scrape as local_scrape
+
 
 app_name = "scraper"
 
 urlpatterns = [
-    path("<str:code>/locally", local_scrape, name="local_scrape"),
-    # path("<str:code>/", aws_lambda_scrape, name="aws_lambda_scrape"),
 ]
