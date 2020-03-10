c6d30cb478918674f91778e29df4ecf759cadb77
Anthony <anthony.sherrill@code.berlin>
2020-03-10 15:25:24 +0100

    adaped test to new function name

--- goodbuyDatabase/tests/test_urls.py
+++ goodbuyDatabase/tests/test_urls.py
@@ -1,18 +1,15 @@
 from django.test import TestCase
 from django.urls import reverse, resolve
-from goodbuyDatabase.endpoints import is_in_own_database
+from goodbuyDatabase.endpoints import is_product_db
 from goodbuyDatabase.views import product_list, show_list_of_codes
 
 
 class TestUrls(TestCase):
-   
+    # Resolves checks if the url is called, the assoziated function is found
+    def test_is_product_in_db(self):
+        url = reverse('goodbuyDatabase:is_product_db', args=['4000582185399'])
+        self.assertEqual(resolve(url).func, is_product_db)
 
-   # Resolves checks if the url is called, the assoziated function is found
-
-    def test_is_in_own_database(self):
-        url = reverse('goodbuyDatabase:is_in_own_database', args=['4000582185399'])
-        self.assertEqual(resolve(url).func, is_in_own_database)
-    
     def test_product_list(self):
         url = reverse('goodbuyDatabase:product_list')
         self.assertEqual(resolve(url).func, product_list)
