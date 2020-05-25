5c9d9a05cbba7df5fc88da5f3f491d23b737f466
Anthony <anthony.sherrill@code.berlin>
2020-05-25 10:11:27 +0200

    started creating test for feedbackstring

--- goodbuyDatabase/tests/test_endpoints.py
+++ goodbuyDatabase/tests/test_endpoints.py
@@ -4,26 +4,26 @@
 """
 This test module tests the endpoints of this application.
 """
-import json
 
 from django.contrib.auth.models import User
 from django.test import TestCase
 from django.test.client import RequestFactory
-from goodbuyDatabase.endpoints import endpoint_save_product
+from goodbuyDatabase.endpoints import endpoint_save_product, create_feedback_string
 from goodbuyDatabase.models import Product
 
 
-class SaveProductPOSTEndpointTest(TestCase):
+class TestEndpoints(TestCase):
     def setUp(self):
         self.factory = RequestFactory()
         self.user = User.objects.create_user(username="admin", password="Nix123456")
         Product.objects.create(code=54321)
+        self.mars = Product.objects.create(name="Mars", code="11111")
 
     def test_save_product_endpoint(self):
-        product = {"name": "Snickers", "code": "54321", "brand": "Snocker Inc."}
+        snickers = {"name": "Snickers", "code": "54321", "brand": "Snocker Inc."}
         request = self.factory.post(
             "/admin/goodbuyDatabase/product",
-            data=product,
+            data=snickers,
             content_type="application/json",
         )
         request.user = self.user
@@ -31,3 +31,7 @@ class SaveProductPOSTEndpointTest(TestCase):
         print("Exists? ", Product.objects.filter(code="54321").exists())
         product = Product.objects.get(code="54321")
         self.assertEqual(product.name, "Snickers")
+
+        def test_create_feedback_string(self):
+            response = create_feedback_string()
+            self.assertEqual(response, self.mars)
