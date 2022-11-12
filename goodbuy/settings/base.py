8815ae35a7e1a360a0a026e0195eca0b92d0c120
anthony <anthony.sherrill@code.berlin>
2022-11-12 14:38:00 +0100

    Changed logic for SECRETE_KEY and DEBUG vars

--- goodbuy/settings/base.py
+++ goodbuy/settings/base.py
@@ -21,10 +21,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = os.environ.get(
+    "SECRET_KEY", 
+    "cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag"
+    )
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DEBUG_VALUE") == "True"
+DEBUG = os.environ.get("DEBUG_VALUE", "") == "True"
 
 ALLOWED_HOSTS = [
     "127.0.0.1",
